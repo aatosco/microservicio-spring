141ac096d898c320c138ba86edcb9d9a0bd02952
aatosco <aa.tosco@outlook.com>
2023-05-20 04:03:33 -0400

    se modifica: ("http://localhost:" + port + "/",String.class)).contains("Hello, World");

--- src/test/java/com/example/testingweb/HttpRequestTest.java
+++ src/test/java/com/example/testingweb/HttpRequestTest.java
@@ -21,7 +21,7 @@ public class HttpRequestTest {
 
 	@Test
 	public void greetingShouldReturnDefaultMessage() throws Exception {
-		assertThat(this.restTemplate.getForObject("http://localhost:" + 8085 + "/",
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
 				String.class)).contains("Hello, World");
 	}
 }
