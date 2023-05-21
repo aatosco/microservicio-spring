15405201ea4b93019a522bdb2e5168ab68e88a82
aatosco <120057196+aatosco@users.noreply.github.com>
2023-05-21 14:49:31 -0400

    Update GreetingService.java

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
 	public String greet() {
-		String password = "1234";
 		return "Finaliza el curso DevOps Foundation v4 - Abraham Tosco";
 	}
 }
